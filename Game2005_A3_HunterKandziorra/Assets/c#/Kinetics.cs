a70cd22b542bfd7e7d4a2877aaa395a37c23654c
vashj1995 <vashj1995@yahoo.com>
2021-11-13 20:46:41 -0500

    kinetics started

---
+++ Game2005_A3_HunterKandziorra/Assets/c#/Kinetics.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physiks : MonoBehaviour
+{
+    public float mass = 1.0f;
+    public Vector3 velocity = Vector3.zero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Now moving " + gameObject.name + "!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + (velocity * Time.deltaTime);
+    }
+}
