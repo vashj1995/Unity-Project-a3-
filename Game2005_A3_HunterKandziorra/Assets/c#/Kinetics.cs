6fb7e45e2b01edcaeac64b9560fa62aa4e576407
vashj1995 <vashj1995@yahoo.com>
2021-11-26 17:29:22 -0500

    Finished Kinetics

--- Game2005_A3_HunterKandziorra/Assets/c#/Kinetics.cs
+++ Game2005_A3_HunterKandziorra/Assets/c#/Kinetics.cs
@@ -2,11 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Physiks : MonoBehaviour
+public class Kinetics: MonoBehaviour
 {
     public float mass = 1.0f;
     public Vector3 velocity = Vector3.zero;
-
 
     // Start is called before the first frame update
     void Start()
