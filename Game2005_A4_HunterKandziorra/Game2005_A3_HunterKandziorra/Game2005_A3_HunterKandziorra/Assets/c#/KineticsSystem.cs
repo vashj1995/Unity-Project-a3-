801f489fc364692791dac0bd086cfa7ffafb34b9
vashj1995 <vashj1995@yahoo.com>
2021-12-07 01:52:15 -0500

    update
    changed collision detection update to collisionupdate, also modified the internals of collisionupdate.

--- Game2005_A4_HunterKandziorra/Game2005_A3_HunterKandziorra/Game2005_A3_HunterKandziorra/Assets/c#/KineticsSystem.cs
+++ Game2005_A4_HunterKandziorra/Game2005_A3_HunterKandziorra/Game2005_A3_HunterKandziorra/Assets/c#/KineticsSystem.cs
@@ -19,26 +19,62 @@ public class KineticsSystem : MonoBehaviour
             Kinetiks[i].velocity += gravity * Time.fixedDeltaTime;
         }
 
-        CollisionDetectionUpdate();
+        CollisionUpdate();
     }
 
-    void CollisionDetectionUpdate()
+    void CollisionUpdate()
     {
-        for (int i = 0; i < CollisionShapes.Count; i++)
+        for (int objectIndexA = 0; objectIndexA < Kinetiks.Count; objectIndexA++)
         {
-            for (int j = 0; j < CollisionShapes.Count; j++)
+            for (int objectIndexB = objectIndexA + 1; objectIndexB < Kinetiks.Count; objectIndexB++)
             {
-                CollisionShape shape = CollisionShapes[i].GetCollisionShape();
-                if (shape == CollisionShape.Sphere)
+                Kinetics objectA = Kinetiks[objectIndexA];
+                Kinetics objectB = Kinetiks[objectIndexB];
+
+                if (objectA.shape == null || objectB.shape == null)
                 {
-                    Color colorA = CollisionShapes[i].GetComponent<Renderer>().material.color;
-                    Color colorB = CollisionShapes[j].GetComponent<Renderer>().material.color;
-
-                    CollisionShapes[i].IsCollidingWithSphere((CollisionShapeSphere)CollisionShapes[j]);
-                    CollisionShapes[i].GetComponent<Renderer>().material.color = Color.Lerp(colorA, colorB, 0.1f);
+                    continue;
                 }
-            }
+                if(objectB.shape.GetCollisionShape() == CollisionShape.Sphere)
+                {
+                    bool isOverlapping = objectA.shape.IsCollidingWithSphere((CollisionShapeSphere)objectB.shape);
 
+                    if(isOverlapping)
+                    {
+                        Debug.Log("Collision");
+                        objectA.GetComponent<Renderer>().material.color = new Color (0,1.0f, 1.0f);
+                        objectB.GetComponent<Renderer>().material.color = new Color(0, 1.0f, 1.0f);
+
+                    }
+                }
+
+                ////if both r spheres
+                //if (objectA.shape.GetCollisionShape() == CollisionShape.Sphere && objectB.shape.GetCollisionShape() == CollisionShape.Sphere)
+                //{
+                //    //does Something ig?
+                //    ((CollisionShapeSphere)objectA.shape).IsOverlappingWithSphere((CollisionShapeSphere)objectB.shape);
+                //}
+            }
         }
     }
+
+    //void CollisionDetectionUpdate()
+    //{
+    //    for (int i = 0; i < CollisionShapes.Count; i++)
+    //    {
+    //        for (int j = 0; j < CollisionShapes.Count; j++)
+    //        {
+    //            CollisionShape shape = CollisionShapes[i].GetCollisionShape();
+    //            if (shape == CollisionShape.Sphere)
+    //            {
+    //                Color colorA = CollisionShapes[i].GetComponent<Renderer>().material.color;
+    //                Color colorB = CollisionShapes[j].GetComponent<Renderer>().material.color;
+
+    //                CollisionShapes[i].IsCollidingWithSphere((CollisionShapeSphere)CollisionShapes[j]);
+    //                CollisionShapes[i].GetComponent<Renderer>().material.color = Color.Lerp(colorA, colorB, 0.1f);
+    //            }
+    //        }
+
+    //    }
+    //}
 }
